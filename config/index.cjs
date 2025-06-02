bf55717e674b1f0dfb309237d86be792c4684436
Clearlove3333 <2173180278@qq.com>
2025-06-02 13:59:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,9 +7,9 @@
  */
 const USER_CONFIG = {
   // 请将以下代码按照图示粘贴到对应位置
-USE_PASSAGE: 'wechat-test',
 
-{"APP_ID":"wx012d4aebeb9ff0b0","APP_SECRET":"f14d83198c5047bd4f5194adf755a29f","IS_SHOW_COLOR":false,"CALLBACK_TEMPLATE_ID":"hjFnaMHCZx48oFKFVr0ccmK5jxDJCC1NCfDFWdqbfBg","CALLBACK_USERS":[{"name":"自己","id":"oqW3B7WZDh4hE3vhMgWkHeqWfuD0"}],"USERS":[{"name":"测试用户","id":"oqW3B7QZYoYK5U-jeUOXED7KzAho","useTemplateId":"WgPKnI7how1yfVGBsMd2li_YAQ16YDo5htvZ-pcaMMI","province":"山西","city":"吕梁","horoscopeDate":"06-01","horoscopeDateType":"本月","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"节日","name":"七夕节","date":"07-07","year":"2025"}],"customizedDateList":[]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":false,"earthyLoveWords":false,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":true,"birthdayMessage":true}
+
+USE_PASSAGE: 'wechat-test',"APP_ID":"wx012d4aebeb9ff0b0","APP_SECRET":"f14d83198c5047bd4f5194adf755a29f","IS_SHOW_COLOR":false,"CALLBACK_TEMPLATE_ID":"hjFnaMHCZx48oFKFVr0ccmK5jxDJCC1NCfDFWdqbfBg","CALLBACK_USERS":[{"name":"cy","id":"oqW3B7WZDh4hE3vhMgWkHeqWfuD0"}],"USERS":[{"name":"测试用户","id":"oqW3B7QZYoYK5U-jeUOXED7KzAho","useTemplateId":"WgPKnI7how1yfVGBsMd2li_YAQ16YDo5htvZ-pcaMMI","province":"山西","city":"吕梁","horoscopeDate":"06-01","horoscopeDateType":"本月","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"节日","name":"七夕节","date":"07-07","year":"2025"}],"customizedDateList":[]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":false,"earthyLoveWords":false,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":true,"birthdayMessage":true}
 }
 module.exports = USER_CONFIG
 
