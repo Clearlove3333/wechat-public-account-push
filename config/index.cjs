82a911ac73e3e6c7be0bdf928af6920d8d49f644
Clearlove3333 <2173180278@qq.com>
2025-06-03 08:59:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -10,17 +10,23 @@ const USER_CONFIG = {
 
 
 // USE_PASSAGE: 'wechat-test',
-  USE_PASSAGE: 'push-plus',
-"APP_ID":"","APP_SECRET":"","IS_SHOW_COLOR":false,
-  "CALLBACK_TEMPLATE_ID":"hjFnaMHCZx48oFKFVr0ccmK5jxDJCC1NCfDFWdqbfBg",
-  "CALLBACK_USERS":[
-    {"name":"自己","id":"oqW3B7WZDh4hE3vhMgWkHeqWfuD0"}],
+// "APP_ID":"","APP_SECRET":"","IS_SHOW_COLOR":false,
+//   "CALLBACK_TEMPLATE_ID":"hjFnaMHCZx48oFKFVr0ccmK5jxDJCC1NCfDFWdqbfBg",
+//   "CALLBACK_USERS":[
+//     {"name":"自己","id":"oqW3B7WZDh4hE3vhMgWkHeqWfuD0"}],
+//   "USERS":[
+//     {"name":"测试","id":"oqW3B7QZYoYK5U-jeUOXED7KzAho","useTemplateId":"LemOZLtwPz75uxWD8oeTn2yUl3_RBXfrXGZdCWSfVwE","province":"山西","city":"吕梁","horoscopeDate":"06-09","horoscopeDateType":"今日","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"节日","name":"国庆节","date":"10-01","year":"2025"},{"type":"节日","name":"圣诞节","date":"12-25","year":"2025"}],"customizedDateList":[]
+//                                                                 },
+//     {"name":"自己","id":"oqW3B7WZDh4hE3vhMgWkHeqWfuD0","useTemplateId":"LemOZLtwPz75uxWD8oeTn2yUl3_RBXfrXGZdCWSfVwE","province":"山西","city":"吕梁","horoscopeDate":"06-09","horoscopeDateType":"今日","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"节日","name":"国庆节","date":"10-01","year":"2025"},{"type":"节日","name":"圣诞节","date":"12-25","year":"2025"}],"customizedDateList":[]
+//                                                                 },
+//   ],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":false,"oneTalk":true,"earthyLoveWords":false,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":true,"birthdayMessage":true}
+
+//方糖
+   USE_PASSAGE: 'push-plus',
   "USERS":[
-    {"name":"测试","id":"oqW3B7QZYoYK5U-jeUOXED7KzAho","useTemplateId":"LemOZLtwPz75uxWD8oeTn2yUl3_RBXfrXGZdCWSfVwE","province":"山西","city":"吕梁","horoscopeDate":"06-09","horoscopeDateType":"今日","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"节日","name":"国庆节","date":"10-01","year":"2025"},{"type":"节日","name":"圣诞节","date":"12-25","year":"2025"}],"customizedDateList":[]
+   {"name":"自己","id":"edb7392a53e942ef9cb08e8936d3890d","useTemplateId":"0001","province":"山西","city":"吕梁","horoscopeDate":"06-09","horoscopeDateType":"今日","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"节日","name":"国庆节","date":"10-01","year":"2025"},{"type":"节日","name":"圣诞节","date":"12-25","year":"2025"}],"customizedDateList":[]
                                                                 },
-    {"name":"自己","id":"oqW3B7WZDh4hE3vhMgWkHeqWfuD0","useTemplateId":"LemOZLtwPz75uxWD8oeTn2yUl3_RBXfrXGZdCWSfVwE","province":"山西","city":"吕梁","horoscopeDate":"06-09","horoscopeDateType":"今日","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"节日","name":"国庆节","date":"10-01","year":"2025"},{"type":"节日","name":"圣诞节","date":"12-25","year":"2025"}],"customizedDateList":[]
-                                                                },
-  ],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":false,"oneTalk":true,"earthyLoveWords":false,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":true,"birthdayMessage":true}
+   ],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":false,"oneTalk":true,"earthyLoveWords":false,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":true,"birthdayMessage":true}
 }
 module.exports = USER_CONFIG
 
