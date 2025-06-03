a68718ef901f488f1eaa328a80e94fe7cd708427
Clearlove3333 <2173180278@qq.com>
2025-06-03 10:55:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -55,13 +55,13 @@ const USER_CONFIG = {
     // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: '0002',
 
-  CALLBACK_USERS: [
+  "CALLBACK_TEMPLATE_ID": "0002",
+  "CALLBACK_USERS": [
     {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '4f5b174976394fbb99078c6e03dfcf91',
+      "name": "自己",
+      "id": "4f5b174976394fbb99078c6e03dfcf91"
     }
-  ],
+  ]
 };
 
 module.exports = USER_CONFIG;
