9b4aa57acae39ecc8a5154fb728c60e2c0113562
Clearlove3333 <2173180278@qq.com>
2025-07-30 11:01:37 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -21,7 +21,7 @@ const TEMPLATE_CONFIG = [
     风向：{{wind_direction.DATA}} 
     风级：{{wind_scale.DATA}} 
     温馨提示：{{notice.DATA}} 
-    节日：{{birthday_message.DATA}} 
+    纪念日：{{birthday_message.DATA}} 
     每日一句😋 
     中文：{{note_ch.DATA}} 
     English：{{note_en.DATA}}`
