8a3035672f4e46b225b3f2cb472a04d0b2d1f5c3
Clearlove3333 <2173180278@qq.com>
2025-08-01 11:10:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,26 +1,4 @@
-/* eslint-disable */
-
-/**
- * 此项目配置为方便新人使用，已缩减至最简配置。
- * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
- * 自行添加属性，以支持更多个性化功能
- */
 const USER_CONFIG = {
-  // 请将以下代码按照图示粘贴到对应位置
-
-
-// USE_PASSAGE: 'wechat-test',
-// "APP_ID":"","APP_SECRET":"","IS_SHOW_COLOR":false,
-//   "CALLBACK_TEMPLATE_ID":"hjFnaMHCZx48oFKFVr0ccmK5jxDJCC1NCfDFWdqbfBg",
-//   "CALLBACK_USERS":[
-//     {"name":"自己","id":"oqW3B7WZDh4hE3vhMgWkHeqWfuD0"}],
-//   "USERS":[
-//     {"name":"测试","id":"oqW3B7QZYoYK5U-jeUOXED7KzAho","useTemplateId":"LemOZLtwPz75uxWD8oeTn2yUl3_RBXfrXGZdCWSfVwE","province":"山西","city":"吕梁","horoscopeDate":"06-09","horoscopeDateType":"今日","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"节日","name":"国庆节","date":"10-01","year":"2025"},{"type":"节日","name":"圣诞节","date":"12-25","year":"2025"}],"customizedDateList":[]
-//                                                                 },
-//     {"name":"自己","id":"oqW3B7WZDh4hE3vhMgWkHeqWfuD0","useTemplateId":"LemOZLtwPz75uxWD8oeTn2yUl3_RBXfrXGZdCWSfVwE","province":"山西","city":"吕梁","horoscopeDate":"06-09","horoscopeDateType":"今日","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"节日","name":"国庆节","date":"10-01","year":"2025"},{"type":"节日","name":"圣诞节","date":"12-25","year":"2025"}],"customizedDateList":[]
-//                                                                 },
-//   ],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":false,"oneTalk":true,"earthyLoveWords":false,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":true,"birthdayMessage":true}
-
 //方糖
   USE_PASSAGE: 'push-plus',
   "USERS": [
@@ -129,7 +107,7 @@ const USER_CONFIG = {
           ]
         }
     }
-    ],
+  ],
   "SWITCH": {
     "weather": true,
     "holidaytts": true,
